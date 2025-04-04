a208ba5b1c744fdc7f23802824b9b447fcf201ce
volta <voltasimone1@gmail.com>
2025-04-04 15:29:57 +0200

    Aggiunto driver per lancio di tutti i test insieme

---
+++ Note-project-master/test/runAllTests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by volta on 04/04/2025.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
